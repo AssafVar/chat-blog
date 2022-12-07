9b8eec04a3bff9976f38fcfa2020c60ceb026ed4
assaf21 <assaf@ad-knight.com>
2022-12-07 21:23:26 +0200

    Connect server to react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  const testResponse  = async ()=>{
+    const response = await axios.get('http://localhost:5000/hello');
+    response.data && setMessage(response.data);
+  };
+
+  !message && testResponse();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {message?<h1>{message}</h1>:<h1>Error</h1>}
     </div>
   );
 }
